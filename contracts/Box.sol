fd71af1b0bc61b1a03570e899987345454184bcb
milosdjurica <milosdjurica13@gmail.com>
2023-12-17 14:36:16 +0100

    added box contract and installed openzeppelin/contracts package

---
+++ contracts/Box.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+////////////////////
+// * Imports 	  //
+////////////////////
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
+	////////////////////
+	// * Variables	  //
+	////////////////////
+	uint private value;
+
+	////////////////////
+	// * Events 	  //
+	////////////////////
+	event ValueChanged(uint newValue);
+
+	////////////////////
+	// * Functions	  //
+	////////////////////
+	function store(uint _newValue) public onlyOwner {
+		value = _newValue;
+		emit ValueChanged(_newValue);
+	}
+
+	function retreive() public view returns (uint) {
+		return value;
+	}
+}
