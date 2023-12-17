03bd95a3acf53efe5719c0ccd1a56415acdbbb25
milosdjurica <milosdjurica13@gmail.com>
2023-12-18 00:26:09 +0100

    updated GovernanceToken to match newest version, and added GovernorContract

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -1,33 +1,29 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract GovernanceToken is ERC20Votes {
-	uint256 public s_maxSupply = 1000000000000000000000000;
-
+contract GovernanceToken is ERC20, ERC20Permit, ERC20Votes {
 	constructor()
-		ERC20("GovernanceToken", "GT")
+		ERC20("GovernanceToken", "MTK")
 		ERC20Permit("GovernanceToken")
-	{
-		_mint(msg.sender, s_maxSupply);
-	}
+	{}
 
-	// Overrides
+	// The following functions are overrides required by Solidity.
 
-	function _afterTokenTransfer(
+	function _update(
 		address from,
 		address to,
-		uint amount
-	) internal override(ERC20Votes) {
-		super._afterTokenTransfer(from, to, amount);
+		uint256 value
+	) internal override(ERC20, ERC20Votes) {
+		super._update(from, to, value);
 	}
 
-	function _mint(address to, uint amount) internal override(ERC20Votes) {
-		super._mint(to, amount);
-	}
-
-	function _burn(address account, uint amount) internal override(ERC20Votes) {
-		super._burn(account, amount);
+	function nonces(
+		address owner
+	) public view override(ERC20Permit, Nonces) returns (uint256) {
+		return super.nonces(owner);
 	}
 }
