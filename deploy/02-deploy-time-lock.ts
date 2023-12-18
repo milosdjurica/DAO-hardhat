8c33aefab179340bf9c5483ed0724806cef48821
milosdjurica <milosdjurica13@gmail.com>
2023-12-18 01:23:57 +0100

    02-deploy-time-lock script t

---
+++ deploy/02-deploy-time-lock.ts
@@ -0,0 +1,25 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+import { MIN_DELAY } from "../utils/helper.config";
+
+const deployTimeLock: DeployFunction = async function (
+	hre: HardhatRuntimeEnvironment,
+) {
+	const { deployer } = await hre.getNamedAccounts();
+	const { deploy, log } = hre.deployments;
+	const ethers = hre.ethers;
+
+	const timeLock = await deploy("TimeLock", {
+		from: deployer,
+		args: [MIN_DELAY, [], []],
+		log: true,
+	});
+
+	log(`TimeLock contract: `, timeLock.address);
+	log("----------------------------------------------------");
+};
+
+export default deployTimeLock;
+deployTimeLock.id = "deployTimeLock"; // id required to prevent re-execution
+deployTimeLock.tags = ["timeLock", "all"];
--- utils/helper.config.ts
+++ utils/helper.config.ts
@@ -1,1 +1,4 @@
 export const developmentChains = ["hardhat", "localhost", "ganache"];
+
+// 1 hour
+export const MIN_DELAY = 3600;
