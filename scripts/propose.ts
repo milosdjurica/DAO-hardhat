335c4e73f396d7982ff766f2656e17f4a9a54815
milosdjurica <milosdjurica13@gmail.com>
2023-12-18 13:57:51 +0100

    move-blocks function

--- scripts/propose.ts
+++ scripts/propose.ts
@@ -1,8 +1,9 @@
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 import {
 	FUNC_NAME,
 	NEW_STORE_VALUE,
 	PROPOSAL_DESCRIPTION,
+	developmentChains,
 } from "../utils/helper.config";
 
 export async function propose(
@@ -10,6 +11,8 @@ export async function propose(
 	args: any[],
 	proposalDescription: string,
 ) {
+	const isDevelopmentChain = developmentChains.includes(network.name);
+
 	const governor = await ethers.getContract("GovernorContract");
 	const box = await ethers.getContract("Box");
 
@@ -22,6 +25,17 @@ export async function propose(
 		`Proposing ${functionToCall} on ${await box.getAddress()} with ${args}`,
 	);
 	console.log(`Proposal description -> ${proposalDescription}`);
+
+	const proposeTx = await governor.propose(
+		[await box.getAddress()],
+		[0],
+		[encodedFunctionCall],
+		proposalDescription,
+	);
+	await proposeTx.wait(1);
+
+	if (isDevelopmentChain) {
+	}
 }
 
 propose(FUNC_NAME, [NEW_STORE_VALUE], PROPOSAL_DESCRIPTION)
