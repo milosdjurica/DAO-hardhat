a20e1534078502aaa8e8f4a8e3340caa147a4d82
milosdjurica <milosdjurica13@gmail.com>
2024-05-05 00:08:21 +0200

    deploy my governor

---
+++ deploy/03-deploy-MyGovernor.ts
@@ -0,0 +1,40 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { TimeLock } from "../typechain-types";
+import { ZERO_ADDRESS } from "../utils/helper.config";
+const deployMyGovernor: DeployFunction = async function (
+	hre: HardhatRuntimeEnvironment,
+) {
+	const { ethers, getNamedAccounts, deployments } = hre;
+	const { deployer } = await getNamedAccounts();
+	const { deploy, log, get } = deployments;
+
+	const votingToken = await get("VotingToken");
+	const timeLock = await get("TimeLock");
+
+	const timeLockContract: TimeLock = (await ethers.getContractAt(
+		"TimeLock",
+		timeLock.address,
+	)) as unknown as TimeLock;
+
+	const myGovernor = await deploy("MyGovernor", {
+		from: deployer,
+		args: [votingToken.address, timeLock.address], // ! constructor args
+		log: true,
+		// waitConfirmations: 1,
+	});
+
+	const proposerRole = await timeLockContract.PROPOSER_ROLE();
+	const executorRole = await timeLockContract.EXECUTOR_ROLE();
+	const adminRole = await timeLockContract.DEFAULT_ADMIN_ROLE();
+	await timeLockContract.grantRole(proposerRole, myGovernor.address);
+	await timeLockContract.grantRole(executorRole, ethers.ZeroAddress);
+	await timeLockContract.revokeRole(adminRole, deployer);
+
+	log(`MyGovernor contract: `, myGovernor.address);
+	log("----------------------------------------------------");
+};
+
+export default deployMyGovernor;
+deployMyGovernor.id = "deployMyGovernor"; // id required to prevent re-execution
+deployMyGovernor.tags = ["MyGovernor", "all"];
