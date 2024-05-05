7f7c7bdf096b130a44fc49894447fcc54fec5590
milosdjurica <milosdjurica13@gmail.com>
2024-05-05 23:10:59 +0200

    started unit tets

--- test/unit/Example.unit.test.ts
+++ test/unit/Example.unit.test.ts
@@ -9,9 +9,22 @@ const isDevelopmentChain = developmentChains.includes(network.name);
 !isDevelopmentChain
 	? describe.skip
 	: describe("Example Unit Tests", () => {
+			const { deploy, log, getDeploymentsFromAddress } = deployments;
+			const CHAIN_ID = network.config.chainId;
+
+			let accounts: HardhatEthersSigner[];
+			let deployer: HardhatEthersSigner;
+			let player1: HardhatEthersSigner;
+
 			beforeEach(async () => {
-				describe("Constructor Tests", () => {
-					it("Example test", async () => {});
-				});
+				await deployments.fixture(["all"]);
+
+				accounts = await ethers.getSigners();
+				deployer = accounts[0];
+				player1 = accounts[1];
 			});
-	  });
+
+			describe("Constructor Tests", () => {
+				it("Example test", async () => {});
+			});
+		});
